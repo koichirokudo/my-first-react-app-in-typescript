f3fc3895cf41653f80d6bae83e3c101408b57384
koichiro kudo <38064137+koichirokudo746@users.noreply.github.com>
2021-03-03 07:08:54 +0900

    basic anotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+// 入門
+// 基本的な型制約のやり方
+// const App = ({ message }: { message: AppProps }) => {
+//   return <div>{message}</div>;
+// };
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+// 一般的には interface に型を書いてそれを使うケースが多い
+interface AppProps {
+  message: string;
+}
+const App = ({ message }: AppProps) => {
+  return <div>{message}</div>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // react 開発に必須
+import ReactDOM from 'react-dom'; // 開発に必須
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // App 内部に対して message を入力した IntrinsicAttributes は本来備わっているという意味
+  // key はエラーがでない attribute の定義に key が定義されているから
+  <App message="Hello, React!" />,
+  document.getElementById('root')
+);
